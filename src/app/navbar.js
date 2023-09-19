9eaa093d67cc46194f75781080e59703a81748d6
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2023-09-19 23:12:41 +0530

    minor changes

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -3,7 +3,7 @@ import Lin from "./li_co";
 
 const Navbar = ()=>{
    return(
-    <div className="text-black text-right mt-8 text-base font-bold mr-20">
+    <div className="text-black text-right mt-8 text-base font-bold mr-20 pb-8">
     <Lin name ="HOME" link = "./link" />
     <Lin name ="ABOUT" link = "./link" />
     <Lin name ="CONTACT" link = "./link" />
