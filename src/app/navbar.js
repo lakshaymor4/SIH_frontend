24291ba0ce5651a56589a9c1dcf076f936bb8547
Suhani Sengar <139343269+ssuhanii@users.noreply.github.com>
2023-09-23 02:52:16 +0530

    my second commit yayyy woohooo

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Lin from "./li_co";
+import logo from "../assets/images/logo2web.png"
 
 const Navbar = () => {
    return (
-      <div className="flex flex-row-reverse text-black text-right mt-2 text-base font-bold pt-2 pb-3 sticky top-0 bg-white">
-         <Lin name="MY PROFILE" link="./profile" />
-         <Lin name="TEST" link="./test" />
-         <Lin name="ABOUT" link="./about" />
-         <Lin name="HOME" link="./" />
+      <>
+         <div className="flex flex-row justify-between sticky top-0 bg-white">
+            <img src={logo} alt="Logo" className="logowebsite" />
+            <span class="flex flex-row text-3xl absolute left-20 top-5 h-20 w-20 dark:text-black font-bold">OptiMed</span>
+            <div className="flex flex-row-reverse text-black text-right mt-2 text-base font-bold pt-2 pb-3 sticky top-0 bg-white">
+               <Lin name="MY PROFILE" link="./profile" />
+               <Lin name="TEST" link="./test" />
+               <Lin name="ABOUT" link="./about" />
+               <Lin name="HOME" link="./" />
 
+            </div>
 
-
-      </div>
+         </div>
+      </>
    )
 }
 export default Navbar
--- src/app/thir.js
+++ src/app/thir.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Ca from "./ca_th";
 
-const Th = ()=>{
-    return(
+const Th = () => {
+    return (
         <div>
-        <div className="flex felx-row  di ">
-        <Ca name = "X"  word = "Happy Customers" /> 
-        <Ca name = "Y" word = "Happy Doctors" />
-        <Ca name = "Z" word = "Satisfaction" />
-        </div>
-        
+            <div className="flex felx-row  di ">
+                <Ca name="X" word="Happy Customers" />
+                <Ca name="Y" word="Happy Doctors   " />
+                <Ca name="Z" word="Satisfaction    " />
+            </div>
+
         </div>
     )
 }
